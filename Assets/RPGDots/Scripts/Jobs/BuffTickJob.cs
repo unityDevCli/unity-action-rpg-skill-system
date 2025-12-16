e691911a7e69e67986a3a61ca7859352cc463a29
Vũ Mạnh Hà <havm@gemstudio.com>
2025-12-16 14:36:12 +0700

    - update job system

---
+++ Assets/RPGDots/Scripts/Jobs/BuffTickJob.cs
@@ -0,0 +1,21 @@
+﻿using RPGDots.Scripts.ComponentData;
+using Unity.Entities;
+
+namespace RPGDots.Scripts.Jobs
+{
+    public partial struct BuffTickJob : IJobEntity
+    {
+        public float DeltaTime;
+
+        public void Execute(DynamicBuffer<BuffElement> buffer)
+        {
+            for (var i = buffer.Length - 1; i >= 0; i--)
+            {
+                var buff = buffer[i];
+                buff.Duration -= DeltaTime;
+                if (buff.Duration <= 0) buffer.RemoveAt(i);
+                else buffer[i] = buff;
+            }
+        }
+    }
+}
--- Assets/RPGDots/Scripts/Systems/BuffSystem.cs
+++ Assets/RPGDots/Scripts/Systems/BuffSystem.cs
@@ -1,4 +1,5 @@
 ﻿using RPGDots.Scripts.ComponentData;
+using RPGDots.Scripts.Jobs;
 using Unity.Burst;
 using Unity.Entities;
 
@@ -15,16 +16,10 @@ namespace RPGDots.Scripts.Systems
         public void OnUpdate(ref SystemState state)
         {
             var dt = SystemAPI.Time.DeltaTime;
-            foreach (var (buffer, entity) in SystemAPI.Query<DynamicBuffer<BuffElement>>().WithEntityAccess())
+            new BuffTickJob()
             {
-                for (var i = buffer.Length - 1; i >= 0; i--)
-                {
-                    var buff = buffer[i];
-                    buff.Duration -= dt;
-                    if (buff.Duration <= 0) buffer.RemoveAt(i);
-                    // else buffer[i] = buff;
-                }
-            }
+                DeltaTime = dt
+            }.Schedule();
         }
 
         [BurstCompile]
