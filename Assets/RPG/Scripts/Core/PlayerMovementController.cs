b574c3725ea12795cebae45e41910779ca8396e1
Vũ Mạnh Hà <havm@gemstudio.com>
2025-12-16 16:01:44 +0700

    - update auto attack

--- Assets/RPG/Scripts/Core/PlayerMovementController.cs
+++ Assets/RPG/Scripts/Core/PlayerMovementController.cs
@@ -1,24 +1,27 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Health))]
-public class PlayerMovement4Dir : MonoBehaviour
+namespace RPG.Scripts.Core
 {
-    [SerializeField] private float moveSpeed = 6f;
-
-    private void Update()
+    [RequireComponent(typeof(Health))]
+    public class PlayerMovement4Dir : MonoBehaviour
     {
-        float x = Input.GetAxisRaw("Horizontal"); // A/D, Left/Right
-        float z = Input.GetAxisRaw("Vertical");   // W/S, Up/Down
+        [SerializeField] private float moveSpeed = 6f;
 
-        var input = new Vector3(x, 0f, z);
-        if (input.sqrMagnitude > 1f) input.Normalize();
-
-        transform.position += input * (moveSpeed * Time.deltaTime);
-
-        // Quay mặt theo hướng di chuyển (nếu đang di chuyển)
-        if (input.sqrMagnitude > 0.0001f)
+        private void Update()
         {
-            transform.forward = Vector3.Lerp(transform.forward, input, 20f * Time.deltaTime);
+            float x = Input.GetAxisRaw("Horizontal"); // A/D, Left/Right
+            float z = Input.GetAxisRaw("Vertical"); // W/S, Up/Down
+
+            var input = new Vector3(x, 0f, z);
+            if (input.sqrMagnitude > 1f) input.Normalize();
+
+            transform.position += input * (moveSpeed * Time.deltaTime);
+
+            // Quay mặt theo hướng di chuyển (nếu đang di chuyển)
+            if (input.sqrMagnitude > 0.0001f)
+            {
+                transform.forward = Vector3.Lerp(transform.forward, input, 20f * Time.deltaTime);
+            }
         }
     }
 }
