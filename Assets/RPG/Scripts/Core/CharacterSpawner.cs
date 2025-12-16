c018c75fb64a05fe25ba75d782bd9c1f6ea8412d
Vũ Mạnh Hà <havm@gemstudio.com>
2025-12-16 15:22:22 +0700

    - spawn enemy + character

---
+++ Assets/RPG/Scripts/Core/CharacterSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Scripts.Core
+{
+    public class CharacterSpawner : MonoBehaviour
+    {
+        [SerializeField] protected GameObject characterPrefab;
+        [SerializeField] protected Transform spawnPoint;
+
+        [SerializeField] private bool spawnOnStart = true;
+
+        public Transform CurrentCharacter { get; private set; }
+
+        private void Start()
+        {
+            if (spawnOnStart) SpawnCharacter();
+        }
+
+        public void SpawnCharacter()
+        {
+            if(characterPrefab == null || spawnPoint ==null) return;
+            
+            var obj = Instantiate(characterPrefab, spawnPoint.position, spawnPoint.rotation);
+            CurrentCharacter = obj.transform;
+        }
+    }
+}
---
+++ Assets/RPG/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using RPG.Scripts.Core;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace RPG.Scripts.Enemy
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [Header("Spawn Settings")] [SerializeField]
+        protected GameObject enemyPrefab;
+
+        [SerializeField] protected Transform[] spawnPoints;
+
+        [SerializeField] protected float spawnInterval = 1.5f;
+        [SerializeField] protected float maxAlive = 20f;
+
+        [Header("Target Player")] [SerializeField]
+        protected Transform targetPlayer;
+
+        private float _nextSpawnTime;
+        private int _aliveCount;
+
+        private void Start()
+        {
+            _nextSpawnTime = Time.time + spawnInterval;
+        }
+
+        private void Update()
+        {
+            if (enemyPrefab == null || spawnPoints == null) return;
+            if (targetPlayer == null) return;
+
+            if (Time.time >= _nextSpawnTime)
+            {
+                _nextSpawnTime = Time.time + spawnInterval;
+                if (_aliveCount < maxAlive) SpawnOne();
+            }
+        }
+
+        private void SpawnOne()
+        {
+            var spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            var obj = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            var enemyAI = obj.GetComponent<EnemyAI>();
+            if (enemyAI != null) enemyAI.SetTarget(targetPlayer);
+
+            var hp = obj.GetComponent<Health>();
+            if (hp != null)
+            {
+                _aliveCount++;
+                hp.Died += OnEnemyDied;
+            }
+        }
+
+        private void OnEnemyDied(Health healthValue)
+        {
+            healthValue.Died -= OnEnemyDied;
+            _aliveCount = Mathf.Max(0, _aliveCount - 1);
+        }
+    }
+}
