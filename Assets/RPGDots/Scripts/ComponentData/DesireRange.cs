85a003772a670706f846fff43379ebf44f3d99d7
Vũ Mạnh Hà <havm@gemstudio.com>
2025-12-16 17:22:01 +0700

    - add tag and data component

---
+++ Assets/RPGDots/Scripts/ComponentData/DesireRange.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+
+namespace RPGDots.Scripts.ComponentData
+{
+    public struct DesireRange : IComponentData
+    {
+        public float Value;
+        public float Tolerance;
+    }
+}
---
+++ Assets/RPGDots/Scripts/ComponentData/NearestEnemy.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace RPGDots.Scripts.ComponentData
+{
+    public struct NearestEnemy : IComponentData
+    {
+        public Entity Value;
+        public float3 Position;
+        public float Distance;
+    }
+}
