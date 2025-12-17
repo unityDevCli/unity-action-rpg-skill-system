e7cfdfaf7a7696015cbd193cab24c13bbbc25669
Vũ Mạnh Hà <havm@gemstudio.com>
2025-12-17 17:20:27 +0700

    - update player component tag + player authoring

---
+++ Assets/RPG/Scripts/Authoring/PlayerAuthoring.cs
@@ -0,0 +1,23 @@
+﻿using RPG.Scripts.ComponentData;
+using Unity.Entities;
+using UnityEngine;
+
+namespace RPG.Scripts.Authoring
+{
+    public class PlayerAuthoring : MonoBehaviour
+    {
+        public float hp = 100f;
+        public float moveSpeed = 4f;
+
+        private class PlayerAuthoringBaker : Baker<PlayerAuthoring>
+        {
+            public override void Bake(PlayerAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new HealthComponent() { Value = authoring.hp });
+                AddComponent(entity, new MoveSpeedComponent() { Value = authoring.moveSpeed });
+                AddComponent(entity, new PlayerTag());
+            }
+        }
+    }
+}
---
+++ Assets/RPG/Scripts/ComponentData/PlayerTag.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace RPG.Scripts.ComponentData
+{
+    public struct PlayerTag : IComponentData
+    {
+        
+    }
+}
