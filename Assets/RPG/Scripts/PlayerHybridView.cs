35a5f935cf8fabd3e04bb69c6feb7f1f0f6d2692
Vũ Mạnh Hà <havm@gemstudio.com>
2025-12-18 14:55:14 +0700

    - Add Player HybirdView

---
+++ Assets/RPG/Scripts/PlayerHybridView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RPG.Scripts.ComponentData;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace RPG.Scripts
+{
+    public class PlayerHybridView : MonoBehaviour
+    {
+        public string speedParams = "Speed";
+        public float damp = 0.1f;
+        private EntityManager _entityManager;
+        private EntityQuery _entityQuery;
+        private Entity _playerEntity;
+
+        private void Start()
+        {
+            _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            _entityQuery = _entityManager.CreateEntityQuery(ComponentType.ReadOnly<PlayerTag>(),
+                ComponentType.ReadOnly<LocalTransform>());
+        }
+
+        private void LateUpdate()
+        {
+            if (_playerEntity == Entity.Null || !_entityManager.Exists(_playerEntity))
+            {
+                if (_entityQuery.IsEmptyIgnoreFilter) return;
+                _playerEntity = _entityQuery.GetSingletonEntity();
+            }
+
+            var lt = _entityManager.GetComponentData<LocalTransform>(_playerEntity);
+
+            transform.SetPositionAndRotation(lt.Position, lt.Rotation);
+        }
+    }
+}
